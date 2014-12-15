925a4c0481852864977bfdf89f52ae372d336063
Megan Gaebler <megan.gaebler@gmail.com>
2014-12-15 12:37:00 -0600

    added computer and human to game class

--- game.rb
+++ game.rb
@@ -2,10 +2,25 @@ require_relative 'board'
 require_relative 'player'
 
 class Game
+  def initialize
+    @players  = players
+  end
 
+  def human
+    @human ||= Human.new
+  end
 
+  def computer
+    @computer ||= ComputerAI.new
+  end
 
+  def players
+    @players ||= [human, computer]
+  end
 
+  def board
+    "A board"
+  end
 
   def mark_space(where)
     where = add_underscore(where)
