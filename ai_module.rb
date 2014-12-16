60e5b9cea371b4a550dc4f72beca49606ade3562
Megan Gaebler <megan.gaebler@gmail.com>
2014-12-16 07:37:25 -0600

    Added first move finder and tests

--- ai_module.rb
+++ ai_module.rb
@@ -2,4 +2,9 @@ module ComputerAI
   def find_empty_spaces(board_grid)
     board_grid.keys.select{ |key| board_grid[key] == " " }
   end
+
+  def first_move(board_grid)
+    return [:center] if board_grid[:center] == " "
+    [:top_left, :top_right, :bottom_left, :bottom_right]
+  end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -6,16 +6,35 @@ end
 
 describe 'Computer' do
   let(:computer){ Computer.new }
+  
+  started_grid = { top_left:    "X", top_center:    " ", top_right:    " ",
+                   middle_left: " ", center:        "O", middle_right: "X",
+                   bottom_left: "X", bottom_center: " ", bottom_right: "O" }
+
+  center_first = { top_left:    " ", top_center:    " ", top_right:    " ",
+                   middle_left: " ", center:        "X", middle_right: " ",
+                   bottom_left: " ", bottom_center: " ", bottom_right: " " }
+
+  corner_first = { top_left:    " ", top_center:    " ", top_right:    " ",
+                   middle_left: " ", center:        " ", middle_right: " ",
+                   bottom_left: " ", bottom_center: " ", bottom_right: "X" }
+  
   it 'uses an "O" as a space' do
     expect(computer.space).to eq("O")
   end
 
-  describe 'find_empty_spaces' do
-    board_grid = { top_left:    "X", top_center:    " ", top_right:    " ",
-                   middle_left: " ", center:        "O", middle_right: "X",
-                   bottom_left: "X", bottom_center: " ", bottom_right: "O" }
-    it 'returns empty spaces from a board grid' do
-      expect(computer.find_empty_spaces(board_grid)).to eq([:top_center, :top_right, :middle_left, :bottom_center])
+  describe 'ComputerAI module' do
+    describe '#find_empty_spaces' do
+      it 'returns empty spaces from a board grid' do
+        expect(computer.find_empty_spaces(started_grid)).to eq([:top_center, :top_right, :middle_left, :bottom_center])
+      end
+    end
+
+    describe '#first_move' do
+      it 'returns the ideal first move (either a corner or center) depending on the human\'s first move' do
+        expect(computer.first_move(center_first)).to eq([:top_left, :top_right, :bottom_left, :bottom_right])
+        expect(computer.first_move(corner_first)).to eq([:center])
+      end
     end
   end
 end
