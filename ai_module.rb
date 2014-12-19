ad8c242c008d082e95d620a23b3a109e0f1cedc2
Megan Gaebler <megan.gaebler@gmail.com>
2014-12-19 11:28:06 -0600

    deleted TODO comments

--- ai_module.rb
+++ ai_module.rb
@@ -30,10 +30,7 @@ module ComputerAI
   end
 
   def defend(all_combinations, marker=space, blank_space=nil)
-    blank_space = marker if blank_space.nil? # the idea is to pass in a " " when you're searching for a move in a started yet sparsely populated board, but you need a test first! Then, up above, save the output of all of these things //(plus the starting move)// and return the one that is a symbol (because it'll return the whole array if it doesn't match anything)
-    # SO: TODO - figure out some strategy past choosing a corner space, like if the computer goes first, it should take a corner. Then, its second move should be the opposite corner. That should actually be true for human moving in a corner, too. Like, that is the best first move, no question. Center is the third-best first move.
-    # TODO - write a method that finds the opposite corner. You already got diagonals. You can do this!
-    # Soon you should make views and test that it works with a dynamically created board
+    blank_space = marker if blank_space.nil? 
     all_combinations.each do |combo|
       if combo.flatten.values_at(1, 3, 5).sort == [" ", blank_space, marker]
         combo.select{ |pair| return pair[0] if pair[1] == " " }
